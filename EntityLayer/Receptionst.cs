074dd35630e5dbadbea26c8788476938636bb979
AdamWadheden <153288071+AdamWadheden@users.noreply.github.com>
2024-03-11 16:03:50 +0100

    Refaktorering av receptionist.cs
    + kommentarer

--- EntityLayer/Receptionst.cs
+++ EntityLayer/Receptionst.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer
+
+    // Här defineras en receptionist. Receptionisten har en unik identifierare i receptionistId,
+    // ett namn, ett lösenord och en lista för att hålla reda på möten kopplade till en specifik receptionist.
+
 {
     public class Receptionist : IUser
     {
@@ -13,6 +17,10 @@ namespace EntityLayer
 
         public string password { get; set; }    
         public List<Appointment> receptionistAppointments { get; set; }
+
+        // Konstruktorn för Receptionisten tar två parametrar (namn och lösenord) och
+        // används för att instansiera objektet. Vid instansiering sätts namn och lösenord,
+        // och en tom lista för möten skapas och kopplas till receptionisten.
 
         public Receptionist(string name, string password)
         {
