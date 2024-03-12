3853390f4c69f849d4b9d8c201a1dcdb266dd0b8
AdamWadheden <153288071+AdamWadheden@users.noreply.github.com>
2024-03-12 15:25:41 +0100

    Revert "Merge pull request #97 from lagerqvisst/CommentCode"
    This reverts commit 668aed2a886903e945884b767bf2cec5fbb221bb, reversing
    changes made to 3b40652534c210bab3da7d67b626853d446a63ef.

--- BusinessLayer/PrescriptionController.cs
+++ BusinessLayer/PrescriptionController.cs
@@ -12,73 +12,43 @@ namespace BusinessLayer
 {
     public class PrescriptionController
     {
-
-        #region UnitOfWork
-
-        private UnitOfWork unitOfWork = new UnitOfWork();
-        private PatientController patientController = new PatientController();
-
-        #endregion UnitOfWork
-
-        /// <summary>
-        /// Skapar och returnerar ett nytt recept med angiven patient, datum och lista av läkemedel.
-        /// Lägger även till läkemedlen i receptet och uppdaterar patientens namn och antalet läkemedel.
-        /// </summary>
-
-        #region Methods
+        UnitOfWork unitOfWork = new UnitOfWork();
+        PatientController patientController = new PatientController();
 
         public Prescription CreatePrescription(int patientId, DateTime dateofPrescription, ObservableCollection<Drug> drugs)
         {
-            // Skapar ett nytt recept.
             Prescription prescription = new Prescription(patientId, dateofPrescription);
 
-            // Loopar igenom varje läkemedel och skapar en koppling till receptet.
             foreach (Drug drug in drugs)
             {
                 PrescriptionDrug prescriptionDrug = new PrescriptionDrug(prescription.prescriptionId, drug.DrugId);
                 prescription.PrescriptionDrugs.Add(prescriptionDrug);
             }
 
-            // Sätter patientens namn och antalet läkemedel i receptet.
+            // Använd patientId istället för att tilldela hela patientobjektet
             prescription.SetPatientName(patientController.GetPatientById(patientId).name);
             prescription.SetDrugCount(drugs.Count);
 
-            // Skapar receptet i databasen genom UnitOfWork.
             unitOfWork.CreatePrescription(prescription);
 
             return prescription;
         }
 
-        /// <summary>
-        /// Hämtar alla recept från databasen.
-        /// </summary>
         public List<Prescription> GetAllPrescriptions()
         {
-            return unitOfWork.PrescriptionRepository.Find(unitOfWork => true).ToList();
+            return unitOfWork.PrescriptionRepository.Find(unitOfWork => true).ToList(); 
         }
-
-        /// <summary>
-        /// Hämtar receptshistoriken för en specifik patient.
-        /// </summary>
-        public List<Prescription> GetPatientPrescriptionHistory(int patientId)
+        public List<Prescription> GetPatientPrescriptionHistory(int patientId) 
         {
             return unitOfWork.PrescriptionRepository.Find(p => p.patientId == patientId).ToList();
         }
-
-        /// <summary>
-        /// Hämtar läkemedel baserat på receptets ID.
-        /// </summary>
         public List<Drug> GetDrugsByPrescriptionId(int prescriptionId)
         {
-            // Hämtar alla kopplingar mellan receptet och läkemedel.
             var prescriptionDrugs = unitOfWork.PrescriptionDrugRepository.Find(pd => pd.prescriptionId == prescriptionId).ToList();
-
-            // Hämtar alla läkemedel baserat på de unika ID:n från kopplingarna.
             var drugIds = prescriptionDrugs.Select(pd => pd.drugId).ToList();
             return unitOfWork.DrugRepository.Find(d => drugIds.Contains(d.DrugId)).ToList();
         }
+
+
     }
-
-    #endregion Methods
-
 }
